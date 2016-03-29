e5c682941b9ff486071ebab046d6338370c0b7eb
vtt01uk <vtt01uk@gmail.com>
2016-03-29 13:48:41 +0100

    Adjusted date format

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   has_many :friendships
   has_many :friends, through: :friendships, class_name: "User"
   
-  self.per_page = 3
+  self.per_page = 5
   
   def full_name
     "#{first_name} #{last_name}"
