d985a2d6f3dc43ca75df29c07553a0cafb6dc1bf
vtt01uk <vtt01uk@gmail.com>
2016-03-30 23:20:24 +0100

    Configured Sendgrid on Heroku account. Modified environment.rb, development.rb and production.rb files to accomodate SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,16 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'a-better-u.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }  
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
