3542ec80fcd2e5d2d9a1c6516a578ea48340565b
Matthew Smith <web.matthew.it@gmail.com>
2016-03-31 09:33:08 -0400

    fixed config/assets.rb

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module ABetterU
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    config.assets.initialize_on_precompile = false
+    
     config.serve_static_assets = true
     # -- all .rb files in that directory are automatically loaded.
 
