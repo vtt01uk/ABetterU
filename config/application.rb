e061bfc91f2dcf5722665213a6f46990ae68d535
Matthew Smith <web.matthew.it@gmail.com>
2016-03-31 08:59:46 -0400

    fiixng precompile

--- config/application.rb
+++ config/application.rb
@@ -5,14 +5,14 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.serve_static_assets = true
 
 
 module ABetterU
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
